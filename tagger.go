9a91db785782af89784b0e418e4c4a1673056120
Michael Gasch <embano1@live.com>
2019-01-24 20:52:27 +0100

    Fixes and Feature enhancements in Tagger
    - make logger configurable
    - fix several VC connection issues, incl. REST API
    - add a Close() method to disconnect from VC
    
    Signed-off-by: Michael Gasch <embano1@live.com>

--- tagger.go
+++ tagger.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"log"
 	"net/url"
+	"os"
+
+	"github.com/vmware/govmomi/vim25/soap"
 
 	"github.com/vmware/govmomi/vapi/tags"
 
@@ -19,16 +22,26 @@ type Client struct {
 	vc *govmomi.Client
 	// rest *rest.Client
 	manager *tags.Manager
+	log     Logger
 }
 
-// New creates a tagging client
-func New(ctx context.Context, vc string, insecure bool) (*Client, error) {
-	u, err := url.Parse(vc)
+// Logger is an interface to make logging configurable
+type Logger interface {
+	Printf(format string, v ...interface{})
+}
+
+// New creates a tagging client with a custom logger. If logger is nil, log.Logger will be used.
+func New(ctx context.Context, logger Logger, vc string, vcuser string, vcpass string, insecure bool) (*Client, error) {
+	if logger == nil {
+		logger = log.New(os.Stdout, "", log.LstdFlags)
+	}
+	u, err := soap.ParseURL(vc)
 	if err != nil {
 		log.Printf("could not parse vCenter client URL: %v", err)
 		return nil, err
 	}
 
+	u.User = url.UserPassword(vcuser, vcpass)
 	c, err := govmomi.NewClient(ctx, u, insecure)
 	if err != nil {
 		log.Printf("could not get vCenter client: %v", err)
@@ -36,12 +49,34 @@ func New(ctx context.Context, vc string, insecure bool) (*Client, error) {
 	}
 
 	r := rest.NewClient(c.Client)
+	err = r.Login(ctx, u.User)
+	if err != nil {
+		log.Printf("could not get VAPI REST client: %v", err)
+		return nil, err
+	}
 	tm := tags.NewManager(r)
 
-	return &Client{vc: c, manager: tm}, nil
+	return &Client{vc: c, manager: tm, log: logger}, nil
 }
 
 // TagVM adds a tag to a virtual machine (ManagedObjectReference)
 func (c *Client) TagVM(ctx context.Context, moref *types.ManagedObjectReference, tagID string) error {
 	return c.manager.AttachTag(ctx, tagID, moref)
+}
+
+// Close closes all connections to vCenter
+func (c *Client) Close(ctx context.Context) error {
+	err := c.vc.Logout(ctx)
+	if err != nil {
+		c.log.Printf("could not close connection to vCenter")
+		return err
+	}
+
+	err = c.manager.Logout(ctx)
+	if err != nil {
+		c.log.Printf("could not close VAPI REST connection to vCenter")
+		return err
+	}
+
+	return nil
 }
