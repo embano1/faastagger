21a6379e1a4d56d877e8584df4671aee76a5b33d
Michael Gasch <embano1@live.com>
2019-02-25 14:59:28 +0100

    Changed to full ManagedObjectReference key in JSON
    Signed-off-by: Michael Gasch <embano1@live.com>

--- types.go
+++ types.go
@@ -11,9 +11,9 @@ type InbountEvent struct {
 	Topic    string `json:"topic,omitempty"`
 	Category string `json:"category,omitempty"`
 
-	UserName    string                        `json:"userName,omitempty"`
-	CreatedTime time.Time                     `json:"createdTime,omitempty"`
-	Object      string                        `json:"object,omitempty"`
-	UUID        string                        `json:"uuid,omitempty"`
-	MoRef       *types.ManagedObjectReference `json:"moref,omitempty"`
+	UserName               string                        `json:"userName,omitempty"`
+	CreatedTime            time.Time                     `json:"createdTime,omitempty"`
+	Object                 string                        `json:"object,omitempty"`
+	UUID                   string                        `json:"uuid,omitempty"`
+	ManagedObjectReference *types.ManagedObjectReference `json:"moref,omitempty"`
 }
